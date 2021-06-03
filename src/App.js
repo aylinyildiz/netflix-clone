dc46e7edd9af6e0615fc3d14d0d3d92676cbc206
Aylin Yıldız <aylinyildiz1628@icloud.com>
2021-06-03 11:11:39 +0300

    build movie rows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Row from "./Row";
+import request from "./requests";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix clone</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
     </div>
   );
 }
